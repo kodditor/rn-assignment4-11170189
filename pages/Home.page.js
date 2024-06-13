66f70673f5850ecf6bad0160c2f1dcf7034c06ab
Kwabena Owusu-Darko <kobbyowusudarko@gmail.com>
2024-06-13 03:14:05 +0000

    Updated Home page

--- pages/Home.page.js
+++ pages/Home.page.js
@@ -1,18 +1,22 @@
+import { Text, View, StyleSheet, Image } from 'react-native'
+import SearchBar from '../components/SearchBar.component'
 
-export default function HomePage({ params: { name, email }}){
+export default function HomePage({ route: { params: { name, email} }}){
+
     return (
         <>
-            <View>
-                <View>
+            <View style={styles.pageContainer}>
+                <View style={styles.accountDetailsContainer} >
                     <View>
-                        <Text>{name}</Text>
-                        <Text>{email}</Text>
+                         <Text style={styles.accountName}>{name}</Text>
+                        <Text style={styles.accountEmail}>{email}</Text>
                     </View>
-                    {/* Image */}
+                    <Image
+                        source={require('../assets/accountPhoto.png')}
+                        style={styles.accountPhoto}
+                    />
                 </View>
-                <View>
-                    {/* Search Bar */}
-                </View>
+                <SearchBar />
                 <View>
                     {/* Featured Jobs Flatlist */}
                 </View>
@@ -23,3 +27,33 @@ export default function HomePage({ params: { name, email }}){
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    pageContainer: {
+        color: 'black',
+        display: 'flex',
+        gap: 30,
+        margin: 25,
+        height: '100%',
+        fontFamily: 'sans-serif'
+    },
+    accountDetailsContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+    accountName: {
+        fontSize: 25,
+        fontWeight: '900',
+    },
+    accountEmail: {
+        color: 'grey',
+        fontSize: 16,
+    },
+    accountPhoto: {
+        width: 40,
+        aspectRatio: 1,
+    },
+})
